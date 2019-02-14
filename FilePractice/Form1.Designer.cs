0c66c333a3eea7cabd35cb848d1403c9559e0c3f
Noah Vandervelden <NVandervel@my.gcu.edu>
2019-02-14 14:12:37 -0700

    ALl of the code

--- FilePractice/Form1.Designer.cs
+++ FilePractice/Form1.Designer.cs
@@ -28,13 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.rchTxBx_stringReader = new System.Windows.Forms.RichTextBox();
+            this.btn_showFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // rchTxBx_stringReader
+            // 
+            this.rchTxBx_stringReader.Location = new System.Drawing.Point(117, 145);
+            this.rchTxBx_stringReader.Name = "rchTxBx_stringReader";
+            this.rchTxBx_stringReader.Size = new System.Drawing.Size(525, 382);
+            this.rchTxBx_stringReader.TabIndex = 0;
+            this.rchTxBx_stringReader.Text = "";
+            // 
+            // btn_showFile
+            // 
+            this.btn_showFile.Location = new System.Drawing.Point(873, 346);
+            this.btn_showFile.Name = "btn_showFile";
+            this.btn_showFile.Size = new System.Drawing.Size(348, 181);
+            this.btn_showFile.TabIndex = 1;
+            this.btn_showFile.Text = "Show File";
+            this.btn_showFile.UseVisualStyleBackColor = true;
+            this.btn_showFile.Click += new System.EventHandler(this.btn_showFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1402, 1032);
+            this.Controls.Add(this.btn_showFile);
+            this.Controls.Add(this.rchTxBx_stringReader);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox rchTxBx_stringReader;
+        private System.Windows.Forms.Button btn_showFile;
     }
 }
 
--- FilePractice/Form1.cs
+++ FilePractice/Form1.cs
@@ -12,9 +12,54 @@ namespace FilePractice
 {
     public partial class Form1 : Form
     {
+
+        public static System.IO.StreamWriter sw;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_showFile_Click(object sender, EventArgs e)
+        {
+            sw = System.IO.File.CreateText("FilePractice1.out");
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName);
+                string allText = sr.ReadToEnd();
+               // this.rchTxBx_stringReader.Text = allText;
+
+                string[] words = allText.Split();
+                MessageBox.Show(words.Length + "");
+                int count = 0;
+                string longestWord = "";
+                for (int i = 0; i < words.Length; i++)
+                {
+                    words[i] = words[i].ToLower();
+                    //rchTxBx_stringReader.Text += words[i] + "\n";
+                    sw.Write(words[i] + " ");
+
+                    if(words[i].Length > 3)
+                    {
+                        rchTxBx_stringReader.Text += words[i] + "\n";
+                    }
+
+
+                    if(words[i].Length > 0 && words[i][0] == 'a' ||
+                        words[i][0] == 'e' || words[i][0] == 'i' ||
+                        words[i][0] == 'o' || words[i][0] == 'u')
+                    {
+                        rchTxBx_stringReader.Text += "Vowel word \n";
+                        rchTxBx_stringReader.Text += words[i] + "\n";
+                    }
+                    while(words[i].Length >= count)
+                    {
+                        longestWord = words[i];
+                        count++;
+                    }
+                }
+                rchTxBx_stringReader.Text +="This is the longest word: " +  longestWord + "\n";
+            }
+            sw.Close();
         }
     }
 }
